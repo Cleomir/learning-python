480378ae8c4e6ccd9d45dda9e8563312f0364a46
Cleomir <cleomir.mg@hotmail.com>
2021-05-14 14:17:15 +0300

    Git add booleans

---
+++ booleans.py
@@ -0,0 +1,16 @@
+# booleans are uppercase
+print(True)
+print(False)
+
+# convert value to boolean
+print(bool("Hello"))
+print(bool(15))
+
+# empty array is False
+print(bool([]))
+
+# zero is False
+print(bool(0))
+
+# empty object is False
+print(bool({}))
