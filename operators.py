a3651f90618f643663e85e4ba657ec5666f74368
Cleomir <cleomir.mg@hotmail.com>
2021-05-15 13:47:58 +0300

    Add operators

---
+++ operators.py
@@ -0,0 +1,34 @@
+# floor division (rounds down to nearest integer)
+x = 15
+y = 2
+print(x // y)
+
+# floor division and assignment
+x = 5
+x //= 3
+print(x)
+
+# exponentiation
+x = 2
+y = 3
+print(x ** y)
+
+# &&
+x = 5
+print(x > 3 and x < 10) 
+
+# ||
+print(x > 3 or x > 10)
+
+# !
+print(not(x < 5))
+
+# object comparison
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is z)
+print(x is not y)
+
+# in
+print("banana" in x)
