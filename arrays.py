fffa7e35b9a501719acd584a13f685bc6a471fe7
Cleomir <cleomir.mg@hotmail.com>
2021-05-17 11:47:40 +0300

    Add more array methods

--- arrays.py
+++ arrays.py
@@ -33,3 +33,57 @@ print(fruits[-4:-1])
 # includes
 if "mango" in fruits:
   print("Mango is in the fruits array")
+
+# replace adding items in array
+fruits[1:3] = ["blackcurrant", "watermelon"]
+print(fruits)
+
+# replace removing items in array
+fruits[1:3] = ["watermelon"]
+print(fruits)
+
+# add item to a specific position
+fruits.insert(1, "pineapple")
+print(fruits)
+
+# push
+fruits.append("strawberry")
+print(fruits)
+
+# add array at the end
+moreFruits = ["papaya"]
+fruits.extend(moreFruits)
+print(fruits)
+
+# splice
+fruits.remove("apple")
+print(fruits)
+
+# remove specific index
+fruits.pop(3)
+print(fruits)
+
+# pop
+fruits.pop()
+print(fruits)
+
+# delete array
+# del fruits
+
+# clear array
+# fruits.clear()
+# print(fruits)
+
+# loop through array
+for fruit in fruits:
+  print(fruit)
+
+# loop through array via indexes
+for i in range(len(fruits)):
+  print(fruits[i])
+
+# looping via while
+i = 0
+while i < len(fruits):
+  print(fruits[i])
+  i += 1
