fbed60c5d03fab51603f3eb383dfec2393e04927
Cleomir <cleomir.mg@hotmail.com>
2021-05-19 14:00:14 +0300

    Add tuples

---
+++ tuples.py
@@ -0,0 +1,12 @@
+# one item tuple
+fruitTuple = ("apple",)
+print(type(fruitTuple))
+
+# not a tuple
+fruitTuple = ("apple")
+print(type(fruitTuple))
+
+# tuple constructor
+fruitsTuple = tuple(("apple", "banana", "cherry"))
+print(fruitsTuple)
+print(type(fruitsTuple))
