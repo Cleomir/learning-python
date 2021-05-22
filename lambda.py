de2bc522585fa587a5869fed07314bc4c238d2a5
Cleomir <cleomir.mg@hotmail.com>
2021-05-22 16:13:28 +0300

    Add lambda

---
+++ lambda.py
@@ -0,0 +1,7 @@
+# one argument
+sumLambda = lambda number : number + 10
+print(sumLambda(5))
+
+# multiple arguments
+multiplicationLambda = lambda number1, number2 : number1 * number2
+print(multiplicationLambda(2, 2))
